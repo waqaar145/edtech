ac7faa6d248342c0a5d876ad9cccd9b1299e1eb1
waqaarKeubik <waqaar@unifynd.com>
2021-05-18 13:58:03 +0530

    migration file updated

--- server/db/migrations/20210329105411_create_comments_table.js
+++ server/db/migrations/20210329105411_create_comments_table.js
@@ -5,6 +5,9 @@ exports.up = function(knex, Promise) {
     table.integer('comm_user_id').references('u_id').inTable('ed_users');
     table.integer('comm_parent_id').references('comm_id').inTable('ed_comments');
     table.string('comm_comment', 5000).notNullable();
+    table.integer('comm_total_likes').defaultTo(0);
+    table.integer('comm_total_dislikes').defaultTo(0);
+    table.integer('comm_total_replies').defaultTo(0);
     table.boolean('comm_is_active').defaultTo(true);
     table.boolean('comm_is_deleted').defaultTo(false);
     table.timestamp('comm_created_at').defaultTo(knex.fn.now());
